a712218ac152225d96f2124e9d606d446b7f31a8
z20ist24 <z20ist24@z00p3-08.SPSKNM>
2022-10-18 10:00:18 +0200

    Zmena

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,36 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =`
   <div>
-    <h1>Contrio</h1>
-    <input type="text" placeholder="Search..">
+    <h1 class="headher">Contrio</h1>
+    <div class="headher">
+      <input id="Search" type="text" placeholder="Search..">
+      <button>Search</button>
+    </div>
     <br>
-    <div class="countrybutton">
+    <div class="countrybuttonrow1">
       <button>Country 1</button>
       <button>Country 1</button>
       <button>Country 1</button>
       <button>Country 1</button>
       <button>Country 1</button>
-      <button>Country 1</button>
-    <div>
   </div>`
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '31c5235490msha1fb83c9e91f9d4p193520jsn67bdd3fc1f71',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
+let result;
+function submite() {
+  result = Number(document.getElementById("Search"));
+  console.log(result);
+}
 
-fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));	
+submite();  
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '31c5235490msha1fb83c9e91f9d4p193520jsn67bdd3fc1f71',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+  
+  fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&minPopulation=15000&namePrefix=${text}`, options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
