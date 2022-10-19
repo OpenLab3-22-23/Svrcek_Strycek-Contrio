041fb88ffc94af8e8c0a8d91a7d38a1208d9c46e
Andrej Svrček <nepoviem251@gmail.com>
2022-10-19 20:44:20 +0200

    Fungujúci search bar z buttnom a uprava na Grid.

--- src/main.ts
+++ src/main.ts
@@ -3,35 +3,45 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML =`
   <div>
     <h1 class="headher">Contrio</h1>
-    <div class="headher">
-      <input id="Search" type="text" placeholder="Search..">
-      <button>Search</button>
+    <div class="search-menu">
+      <input class="search-bar" type="text" placeholder="Search.." id="search">
+      <input class="search-button" type="button" id="btn1" value="Search">
     </div>
-    <br>
-    <div class="countrybuttonrow1">
-      <button>Country 1</button>
-      <button>Country 1</button>
-      <button>Country 1</button>
-      <button>Country 1</button>
-      <button>Country 1</button>
+    <div class="country-grid">
+      <div class="country"><a>Country 1</a></div>
+      <div class="country"><a>Country 2</a></div>
+      <div class="country"><a>Country 3</a></div>
+      <div class="country"><a>Country 4</a></div>
+      <div class="country"><a>Country 5</a></div>
+      <div class="country"><a>Country 6</a></div>
+      <div class="country"><a>Country 7</a></div>
+      <div class="country"><a>Country 8</a></div>
+      <div class="country"><a>Country 9</a></div>
+      <div class="country"><a>Country 10</a></div>
+      <div class="country"><a>Country 11</a></div>
+      <div class="country"><a>Country 12</a></div>
+    </div>
   </div>`
 
-let result;
-function submite() {
-  result = Number(document.getElementById("Search"));
-  console.log(result);
+const searchOutput = document.getElementById('search');
+const btn1 = document.getElementById('btn1');
+let text: string = "";
+
+function submit(): void {
+  text = searchOutput.value;
+
+  fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&minPopulation=15000&namePrefix=${text}`, options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
 }
 
-submite();  
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '31c5235490msha1fb83c9e91f9d4p193520jsn67bdd3fc1f71',
-      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
-  };
-  
-  fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&minPopulation=15000&namePrefix=${text}`, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+btn1.addEventListener('click', submit);
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '31c5235490msha1fb83c9e91f9d4p193520jsn67bdd3fc1f71',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
