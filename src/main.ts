10927686596beefa07451b39b26012ab91f5465e
z20ist24 <z20ist24@z00p3-08.SPSKNM>
2022-10-20 09:00:43 +0200

    Update
    Fixed errors, Statrted to Iter Data

--- src/main.ts
+++ src/main.ts
@@ -9,39 +9,40 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML =`
     </div>
     <div class="country-grid">
       <div class="country"><a>Country 1</a></div>
-      <div class="country"><a>Country 2</a></div>
-      <div class="country"><a>Country 3</a></div>
-      <div class="country"><a>Country 4</a></div>
-      <div class="country"><a>Country 5</a></div>
-      <div class="country"><a>Country 6</a></div>
-      <div class="country"><a>Country 7</a></div>
-      <div class="country"><a>Country 8</a></div>
-      <div class="country"><a>Country 9</a></div>
-      <div class="country"><a>Country 10</a></div>
-      <div class="country"><a>Country 11</a></div>
-      <div class="country"><a>Country 12</a></div>
     </div>
   </div>`
 
-const searchOutput = document.getElementById('search');
-const btn1 = document.getElementById('btn1');
+const searchOutput = document.getElementById('search') as HTMLInputElement;
+const btn1 = document.getElementById('btn1') as HTMLInputElement;
 let text: string = "";
 
 function submit(): void {
   text = searchOutput.value;
 
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '31c5235490msha1fb83c9e91f9d4p193520jsn67bdd3fc1f71',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+
   fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&minPopulation=15000&namePrefix=${text}`, options)
   .then(response => response.json())
-  .then(response => console.log(response))
+  .then(response => {
+    response.data.forEach((item, index) => {
+      console.log(`${index + 1}. ${item.name} ${item.country}`)
+    });
+    // Iterovat response 
+
+    // Na kazdom prvku vytovirt HTML DIV Element a obsianut datami z aktualneho prvku v iteracii
+
+    // Na koniec vykreslit vsetky vygenerovane elementy do stranky
+
+    // HTMLElement.appendChild()
+  }
+  )
   .catch(err => console.error(err));
 }
 
 btn1.addEventListener('click', submit);
-
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '31c5235490msha1fb83c9e91f9d4p193520jsn67bdd3fc1f71',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-  }
-};
